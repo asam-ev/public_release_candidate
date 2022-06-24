5c6d7b4b18b3397755173e3785ab8b1e2d00154b
Philip Windecker <95633467+philipwindecker@users.noreply.github.com>
2022-06-24 15:21:22 +0200

    Update run-build.sh
    test

--- run-build.sh
+++ run-build.sh
@@ -2,6 +2,7 @@
 # This script is used tackle a some specifics of the GitHub action used with a docker container. 
 # It copies the cached node-modules from the container to the runner workspace and then triggers the build using npm
 
+ls
 cp -r /usr/src/repo/. /github/workspace/.
 cd repo
 exec antora --stacktrace --fetch --clean site.yml
